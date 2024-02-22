e7732231119d9d389d7a977bff252ff71b6eb271
MaarifSariyev <maarifsariyev5@gmail.com>
2024-02-22 23:57:45 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,94 @@
+let country_code = { "AED" : "AE", "AFN" : "AF", "XCD" : "AG", "ALL" : "AL", "AMD" : "AM", "ANG" :
+"AN", "AOA" : "AO", "AQD" : "AQ", "ARS" : "AR", "AUD" : "AU", "AZN" : "AZ", "BAM" : "BA",
+"BBD" : "BB", "BDT" : "BD", "XOF" : "BE", "BGN" : "BG", "BHD" : "BH", "BIF" : "BI", "BMD" :
+"BM", "BND" : "BN", "BOB" : "BO", "BRL" : "BR", "BSD" : "BS", "NOK" : "BV", "BWP" : "BW",
+"BYR" : "BY", "BZD" : "BZ", "CAD" : "CA", "CDF" : "CD", "XAF" : "CF", "CHF" : "CH", "CLP" :
+"CL", "CNY" : "CN", "COP" : "CO", "CRC" : "CR", "CUP" : "CU", "CVE" : "CV", "CYP" : "CY",
+"CZK" : "CZ", "DJF" : "DJ", "DKK" : "DK", "DOP" : "DO", "DZD" : "DZ", "ECS" : "EC", "EEK" :
+"EE", "EGP" : "EG", "ETB" : "ET", "EUR" : "FR", "FJD" : "FJ", "FKP" : "FK", "GBP" : "GB", "GEL"
+: "GE", "GGP" : "GG", "GHS" : "GH", "GIP" : "GI", "GMD" : "GM", "GNF" : "GN", "GTQ" : "GT",
+"GYD" : "GY", "HKD" : "HK", "HNL" : "HN", "HRK" : "HR", "HTG" : "HT", "HUF" : "HU", "IDR" :
+"ID", "ILS" : "IL", "INR" : "IN", "IQD" : "IQ", "IRR" : "IR", "ISK" : "IS", "JMD" : "JM", "JOD" : "JO",
+"JPY" : "JP", "KES" : "KE", "KGS" : "KG", "KHR" : "KH", "KMF" : "KM", "KPW" : "KP", "KRW" :
+"KR", "KWD" : "KW", "KYD" : "KY", "KZT" : "KZ", "LAK" : "LA", "LBP" : "LB", "LKR" : "LK", "LRD"
+: "LR", "LSL" : "LS", "LTL" : "LT", "LVL" : "LV", "LYD" : "LY", "MAD" : "MA", "MDL" : "MD", "MGA" :
+"MG", "MKD" : "MK", "MMK" : "MM", "MNT" : "MN", "MOP" : "MO", "MRO" : "MR", "MTL" : "MT",
+"MUR" : "MU", "MVR" : "MV", "MWK" : "MW", "MXN" : "MX", "MYR" : "MY", "MZN" : "MZ", "NAD"
+: "NA", "XPF" : "NC", "NGN" : "NG", "NIO" : "NI", "NPR" : "NP", "NZD" : "NZ", "OMR" : "OM",
+"PAB" : "PA", "PEN" : "PE", "PGK" : "PG", "PHP" : "PH", "PKR" : "PK", "PLN" : "PL", "PYG" :
+"PY", "QAR" : "QA", "RON" : "RO", "RSD" : "RS", "RUB" : "RU", "RWF" : "RW", "SAR" : "SA",
+"SBD" : "SB", "SCR" : "SC", "SDG" : "SD", "SEK" : "SE", "SGD" : "SG", "SKK" : "SK", "SLL" :
+"SL", "SOS" : "SO", "SRD" : "SR", "STD" : "ST", "SVC" : "SV", "SYP" : "SY", "SZL" : "SZ", "THB"
+: "TH", "TJS" : "TJ", "TMT" : "TM", "TND" : "TN", "TOP" : "TO", "TRY" : "TR", "TTD" : "TT",
+"TWD" : "TW", "TZS" : "TZ", "UAH" : "UA", "UGX" : "UG", "USD" : "US", "UYU" : "UY", "UZS" :
+"UZ", "VEF" : "VE", "VND" : "VN", "VUV" : "VU", "YER" : "YE", "ZAR" : "ZA", "ZMK" : "ZM",
+"ZWD" : "ZW" }
+
+
+
+const dropList = document.querySelectorAll(".drop-list select"),
+getButton = document.querySelector("form button");
+fromCurrency = document.querySelector(".from select")
+toCurrency = document.querySelector(".to select")
+for (let index = 0; index < dropList.length; index++) {
+    for( currency_code in country_code){
+        let selected;
+        if(index==0){
+            selected = currency_code == "USD" ? "selected" : ""
+        }else if(index==1){
+            selected = currency_code == "AZN" ? "selected" : ""
+        }
+      let optionTag = `<option value="${currency_code}"${selected}>${currency_code}</option>`;
+      dropList[index].insertAdjacentHTML("beforeend", optionTag);
+
+    }
+    dropList[index].addEventListener("change", e =>{
+        loadFlag(e.target)
+    })
+}
+function loadFlag(Element){
+    for(code in country_code){
+        if(code == Element.value){
+            let imgTag = Element.parentElement.querySelector("img")
+            imgTag.src = `https://flagsapi.com/${country_code[code]}/flat/64.png`
+        }
+    }
+}
+window.addEventListener("onload", () =>{
+    getExchangeRate();
+})
+
+getButton.addEventListener("click", e =>{
+    e.preventDefault();
+    getExchangeRate();
+})
+
+const exchnangeIcon = document.querySelector(".drop-list .icon")
+exchnangeIcon.addEventListener("click", ()=>{
+    let tempCode = fromCurrency.value
+    fromCurrency.value = toCurrency.value
+    toCurrency.value = tempCode;
+    loadFlag(fromCurrency)
+    loadFlag(toCurrency)
+    getExchangeRate()
+})
+
+ function getExchangeRate(){
+    const amount = document.querySelector(".amount input"),
+    exchangeRateTxt = document.querySelector(".exchange-rate");
+    let amountVal = amount.value
+    if(amountVal == ""|| amountVal == "0"){
+        amount.value = "1";
+        amountVal = 1
+    }
+    exchangeRateTxt.innerHTML = "Getting exchange rate..."
+    let url = `https://v6.exchangerate-api.com/v6/25edf166728d6ee64a6defee/latest/${fromCurrency.value}`
+    fetch(url).then(respons => respons.json()).then(result =>{
+        let exchangeRate = result.conversion_rates[toCurrency.value];
+        let totalExchangeRate = (amountVal*exchangeRate).toFixed(2)
+        exchangeRateTxt.innerHTML = `${amountVal} ${fromCurrency.value} = ${totalExchangeRate} ${toCurrency.value}`
+
+    })
+ };
+
+
